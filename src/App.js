290faf01fae723ccb4030680f94fdec7e8bb014a
JDASL <127839733+JDASL@users.noreply.github.com>
2025-01-08 14:25:09 +0100

    added from vscode to github

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import Weather from "./Weather";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from react</h1>
+        <h3>from Jay</h3>
+        <Weather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Hello from Weather</h2>
+      </header>
+    </div>
+  );
+}
